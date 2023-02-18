a08011a5a02b9ae8ef3c7fd60bfcb0ba45e9ff9a
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2023-02-18 13:24:42 +0500

    Lecture 6 & 7
    App design & logic.

--- dice_app/lib/main.dart
+++ dice_app/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +17,49 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: GestureDetector(
+                child: Image.asset(
+                  'images/dice$leftDiceNumber.png',
+                ),
+                onTap: () {
+                  setState(() {
+                    leftDiceNumber = Random().nextInt(6) + 1;
+                  });
+                  // print("Left Dice Clicked and new number is: $leftDiceNumber");
+                },
+              ),
+            ),
+          ),
+          Expanded(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Image.asset(
+                'images/dice2.png',
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
