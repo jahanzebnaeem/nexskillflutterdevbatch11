5e20e720716552bf1faf269fdd7006cef6150d1d
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2023-03-05 13:25:29 +0500

    Lecture 12
    Quizzler app setup and logic building. Understanding lists in dart.

--- quizzler_app/lib/main.dart
+++ quizzler_app/lib/main.dart
@@ -25,6 +25,27 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  List<Icon> questionAnswers = [
+    Icon(
+      Icons.check,
+      color: Colors.green,
+      size: 30.0,
+    ),
+    Icon(
+      Icons.close,
+      color: Colors.red,
+      size: 30.0,
+    ),
+  ];
+
+  int questionNumber = 0;
+
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.'
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +58,8 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                //'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -62,7 +84,11 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-//The user picked true.
+                //The user picked true.
+                setState(() {
+                  questionNumber++;
+                });
+                print('user pressed true for question no $questionNumber');
               },
             ),
           ),
@@ -82,16 +108,24 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-//The user picked false.
+                //The user picked false.
+                print('user pressed false');
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: questionAnswers,
+        )
       ],
     );
   }
 }
+
+/* 1) List to check how many ans are correct
+   2) variable to track my progress
+   3) function to check the entry.
+ */
 
 /*
 question1: 'You can lead a cow down stairs but not up stairs.', false,
