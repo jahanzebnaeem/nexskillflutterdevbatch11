a69e47baeb68511c14011fbf03e5979248290aa6
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2023-03-19 11:25:08 +0500

    Lecture 14
    Start of Flash Chat project initial commit.

---
+++ flash_chat_app/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flash_chat_app/screens/welcome_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(FlashChat());
+
+class FlashChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark().copyWith(
+        textTheme: TextTheme(
+          bodyText2: TextStyle(color: Colors.black54),
+        ),
+      ),
+      home: WelcomeScreen(),
+    );
+  }
+}
